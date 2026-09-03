2e28feaf4dab7e4267fbfb69fb2bde0abc98f6fd
tabishshowkat770-code <tabishshowkat770@gmail.com>
2026-09-03 18:24:24 +0530

    Refactor greeting logic in webapp.py
    Refactored the main function to greet the user directly when the name is provided.

--- webapp.py
+++ webapp.py
@@ -3,11 +3,12 @@ import streamlit as st
 
 st.title("My Simple web app")
 st.write("hi this is my simple app are you agree with it man ? ")
-st.text_input("Enter your name", key="name")
+name = st.text_input("Enter your name", key="name")
 
+def greet():
+    st.write(f"Hello, {name}! Welcome to my simple web app.")
 
-def main():
-    if st.button("Submit"):
-        name = st.session_state.name
-        st.write(f"Hello, {name}! Welcome to my simple web app.")
-st.button("Submit", on_click=main)  
+if name:
+    greet() 
+st.button("Submit", on_click=greet)
+
